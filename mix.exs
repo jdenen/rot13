62c81f35117c43aade6aa6f067022011e3417145
Johnson Denen <johnson.denen@gmail.com>
2019-01-15 23:20:50 -0500

    Update package information

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Rot13.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: "A rot13 cipher library"
     ]
   end
 
@@ -21,8 +23,15 @@ defmodule Rot13.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Johnson Denen"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/jdenen/rot13"}
     ]
   end
 end
