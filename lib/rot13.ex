08371bc7a9cf609b5a5cbf399717633548e8a0b3
Johnson Denen <johnson.denen@gmail.com>
2019-01-15 21:37:48 -0500

    Initial mix project commit

---
+++ lib/rot13.ex
@@ -0,0 +1,18 @@
+defmodule Rot13 do
+  @moduledoc """
+  Documentation for Rot13.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rot13.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
